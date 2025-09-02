b8041d8db3460e553cf1e6071841c3bb4909a28c
Andrew Wiebe <cosmiccollectors131@gmail.com>
2025-09-02 08:28:49 -0500

    Refactor login logic to use dbLoginCheck function
    Moved user authentication logic into a new dbLoginCheck function for better separation of concerns. This prepares the code for future integration with a real database by isolating the login check.

--- Backend/handlers/login.go
+++ Backend/handlers/login.go
@@ -7,26 +7,28 @@ import (
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
-	mockUserList := map[string]string{
-		"user1": "password1",
-		"user2": "password2",
-	}
+	//TODO: Put a database call here
 
 	fmt.Fprintln(w, "Hello from the login endpoint!")
 
 	if r.Method == http.MethodPost {
 		// Handle login logic here
-		fmt.Fprintln(w, "Logging in...")
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-
-		if storedPassword, ok := mockUserList[username]; ok && storedPassword == password {
+		if dbLoginCheck(r.FormValue("username"), r.FormValue("password")) {
 			fmt.Fprintln(w, "Login successful!")
 		} else {
 			http.Error(w, "Invalid username or password", http.StatusUnauthorized)
 		}
-
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
+}
+
+func dbLoginCheck(username string, password string) bool {
+	// TODO: Implement actual database call
+
+	mockUserList := map[string]string{
+		"user1": "password1",
+		"user2": "password2",
+	}
+	return mockUserList[username] == password
 }
