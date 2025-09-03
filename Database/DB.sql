e37f88a85e2872fa4f6a3e8e87df24e09262e19c
Andrew Wiebe <cosmiccollectors131@gmail.com>
2025-09-03 07:22:55 -0500

    Revise and expand database schema
    Dropped existing tables if they exist and corrected table and column names for consistency (e.g., 'users' to 'user', 'couse_uuit' to 'course_uuid'). Added new tables: user_schedule and course_shedule_association, and updated foreign key references to match the revised schema. These changes improve schema clarity and support user schedules and course associations.

--- Database/DB.sql
+++ Database/DB.sql
@@ -1,28 +1,53 @@
+Drop table if exists course_shedule_association; 
+Drop table if exists user_schedule;
+Drop table if exists course_rating;
+Drop table if exists professor;
+Drop table if exists course;
+Drop table if exists user;
+-- Drop table if exists 
+-- Drop table if exists 
+
+
+
 
 -- Note: uuids are intentionally varchars, this lets us expose them to the user
-Create table users(
+Create table user(
     user_uuid varchar(255) primary key not null,
     username varchar(255) not null,
     psswrd varchar(255) not null,
     salt varchar(255) not null, -- salt for passwords
-)
+);
 
 Create table course(
-    couse_uuit varchar(255) primary key not null,
+    couse_uuid varchar(255) primary key not null,
     code varchar(255) not null,
     title varchar(255) not null,
-)
+);
 
 Create table professor(
     prof_uuid varchar(255) primary key not null,
     prof_name varchar(255) not null,
-)
+);
+
 
 Create table course_rating(
     course_rating_uuid varchar(255) primary key not null,
-    poster_uuid varchar(255) not null references users.user_uuid,
+    poster_uuid varchar(255) not null references user.user_uuid,
+    course_uuid varchar(255) not null references course.course_uuid,
     stars int not null, -- This and hours/week could be nullable
     hours_per_week float not null,
     teacher_uuid references professor.prof_uuid,
     review_text varchar(400),
-)
+);
+
+Create table user_schedule(
+    user_schedule_uuid varchar(255) primary key not null,
+    user_uuid varchar(255) not null references user.user_uuid,
+    schedule_title varchar(200),
+);
+
+Create table course_shedule_association(
+    course_shedule_association_uuid varchar(255) primary key not null,
+    shedule_uuid varchar(255) not null references user_schedule.user_schedule_uuid,
+    course_uuid varchar(255) not null references course.course_uuid
+);
